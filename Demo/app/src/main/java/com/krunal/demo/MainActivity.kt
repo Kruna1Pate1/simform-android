28a8ddc11e85c5b2e3dc76217957ed8f2ffe9e4a
Krunal Patel <kbpatel5802@gmail.com>
2023-02-15 16:50:08 +0530

    Use data binding to display time

--- Demo/app/src/main/java/com/krunal/demo/MainActivity.kt
+++ Demo/app/src/main/java/com/krunal/demo/MainActivity.kt
@@ -2,10 +2,35 @@ package com.krunal.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.krunal.demo.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.Default).launch {
+            while (true) {
+                val time = currTime
+                runOnUiThread {
+                    binding.lblTime.text = getString(R.string.current_time, time)
+                }
+            }
+        }
     }
+
+    private val currTime: String
+        get() {
+            val sdf = SimpleDateFormat("HH:mm:ss")
+            return sdf.format(Date())
+        }
 }
