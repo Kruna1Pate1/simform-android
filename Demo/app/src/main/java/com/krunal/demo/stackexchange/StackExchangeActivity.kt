3b555b21a5a381703780a7ffd333655232339ed9
Krunal Patel <kbpatel5802@gmail.com>
2023-05-16 17:02:47 +0530

    Add: Initial commit

---
+++ Demo/app/src/main/java/com/krunal/demo/stackexchange/StackExchangeActivity.kt
@@ -0,0 +1,14 @@
+package com.krunal.demo.stackexchange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.krunal.demo.R
+
+class StackExchangeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stack_exchange)
+    }
+
+
+}
